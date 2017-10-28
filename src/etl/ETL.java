d09a03a235f458d10ddfda7c1a37493cc1087b7b
Daniel Kucharczyk <sowplvision@gmail.com>
2017-10-28 15:34:56 +0200

    Pre-Extract

--- src/etl/ETL.java
+++ src/etl/ETL.java
@@ -5,6 +5,13 @@
  */
 package etl;
 
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 /**
  *
  * @author lenovo
@@ -14,8 +21,19 @@ public class ETL {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws IOException {
+        Document doc = Jsoup.connect("https://www.ceneo.pl/47629930/opinie-2")
+                .header("Accept-Encoding", "gzip, deflate")
+                .userAgent("Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0")
+                .maxBodySize(0)
+                .timeout(600000)
+                .get();
+
+        Elements html = doc.getAllElements();
+
+        for (Element e : html){
+            System.out.println(e.html());
+        }
     }
     
 }
