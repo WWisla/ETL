b414588e863416f399ebe516a9c437b3c68032c8
Daniel Kucharczyk <sowplvision@gmail.com>
2018-01-03 23:33:57 +0100

    Code refactor

--- maven/src/main/java/com/hd/etl/ETL.java
+++ maven/src/main/java/com/hd/etl/ETL.java
@@ -5,6 +5,7 @@ import javax.swing.text.DefaultCaret;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
@@ -85,51 +86,79 @@ public class ETL extends JFrame implements ActionListener{
         setVisible(true);
     }
 
+    public String extract(){
+        //create ceneo.pl url with product number to extract reviews
+        //"https://www.ceneo.pl/47629930#tab=reviews"
+        String url = "https://www.ceneo.pl/" + product.getText() + "#tab=reviews";
+
+        //extract all review sites for this product number
+        try {
+            docList = Extract.extractDocuments(url);
+        }
+        catch (IOException event){
+            event.printStackTrace();
+        }
+
+        //enable transform
+        transform.setEnabled(true);
+
+        return Extract.extractToString();
+    }
+
+    public String transform(){
+        reviews = Transform.transform(docList);
+
+        //enable load
+        load.setEnabled(true);
+
+        return Transform.transformToString();
+    }
+
+    public String load(){
+        //TODO full load code
+
+        //deleting files after load
+        File reviewsXML = new File("reviews.xml");
+        File extractXML = new File("extract.xml");
+        File transformXML = new File("transform.xml");
+
+        if(reviewsXML.delete() && extractXML.delete() && transformXML.delete()){
+            System.out.println("TEMPORARY FILES HAS BEEN DELETED");
+        }
+        else {
+            System.out.println("ERROR");
+        }
+
+        transform.setEnabled(false);
+
+        return "XD";
+    }
+
     public void actionPerformed(ActionEvent e) {
         //Extract button action
         if(e.getActionCommand().equals(extract.getActionCommand())){
-            //create ceneo.pl url with product number to extract reviews
-            //"https://www.ceneo.pl/47629930#tab=reviews"
-            String url = "https://www.ceneo.pl/" + product.getText() + "#tab=reviews";
-
-            //extract all review sites for this product number
-            try {
-                docList = Extract.extractDocuments(url);
-            }
-            catch (IOException event){
-                event.printStackTrace();
-            }
-
             //display result of extract in GUI
-            result.setText(Extract.extractToString());
-
-            //enable transform
-            transform.setEnabled(true);
+            result.setText(extract());
         }
         //Transform button action
         if(e.getActionCommand().equals(transform.getActionCommand())){
-            reviews = Transform.transform(docList);
-
             //display result of transform in GUI
-            result.setText(Transform.transformToString());
-
-            //enable load
-            load.setEnabled(true);
+            result.setText(transform());
         }
         //Load button action
         if(e.getActionCommand().equals(load.getActionCommand())){
-            //TODO
-            Load.load();
+            //display result of load in GUI
+            result.setText(load());
         }
         //ETL button action
         if(e.getActionCommand().equals(etl.getActionCommand())){
-            //TODO
-            System.out.println("ETL");
+            extract();
+            transform();
+            load();
         }
         //Clear Data Base action
         if(e.getActionCommand().equals(clearDataBase.getActionCommand())){
-            //TODO
-            System.out.println("DROP");
+            //TODO finish this method
             Load.dropDataBase();
         }
     }
